b8bd78e4365283ff230f50e0bd2394c7aed8aca5
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 23:18:24 -0600

    Prueba nueva

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ export default async (req, res) => {
 
   try {
     const apiRes = await fetch(url);
+    if (!apiRes.ok) throw new Error(`HTTP status ${apiRes.status}`);
+    
     const data = await apiRes.json();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send(data);
   } catch (err) {
-    res.status(500).json({ error: 'Error al conectar con la API' });
+    res.status(500).json({ error: 'Error al conectar con la API', details: err.message });
   }
 };
-
