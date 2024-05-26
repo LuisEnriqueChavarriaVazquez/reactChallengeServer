42ff0ac460930d74bfba081cc1298684d68629d7
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 22:21:17 -0600

    Prueba vercel

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const API_SERVICE_URL = "https://dev.obtenmas.com/catom/api/challenge";
 
-app.use('/api', createProxyMiddleware({
-    target: API_SERVICE_URL,
+// Middleware para servir archivos estáticos de React (build folder)
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Proxy para resolver el problema CORS al acceder a la API
+app.use('/catom/api/challenge/banks', createProxyMiddleware({
+    target: 'https://dev.obtenmas.com',
     changeOrigin: true,
     pathRewrite: {
-        [`^/api`]: '',
+        '^/catom/api/challenge/banks': '', // Si necesitas modificar el path aquí
     },
 }));
 
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+// Captura cualquier otra ruta y devuelve el index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
