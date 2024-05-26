9dde049cedb471f2037e0aef897bea2ddc3b297c
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 23:10:29 -0600

    Prueba de server

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
+// api/proxy.js
+import fetch from 'node-fetch';
 
-const app = express();
+export default async (req, res) => {
+  const url = 'https://dev.obtenmas.com/catom/api/challenge/banks';
 
-// Middleware para servir archivos estáticos de React (build folder)
-app.use(express.static(path.join(__dirname, 'build')));
+  try {
+    const apiRes = await fetch(url);
+    const data = await apiRes.json();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Error al conectar con la API' });
+  }
+};
 
-// Proxy para resolver el problema CORS al acceder a la API
-app.use('/catom/api/challenge/banks', createProxyMiddleware({
-    target: 'https://dev.obtenmas.com',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/catom/api/challenge/banks': '', // Si necesitas modificar el path aquí
-    },
-}));
-
-// Captura cualquier otra ruta y devuelve el index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
