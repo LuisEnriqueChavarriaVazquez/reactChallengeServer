16bbf5e1bb9edb56bd493a15348d44ad1b906878
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2024-05-25 23:31:39 -0600

    Elimine import porque no funcionaba

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 // api/proxy.js
-import fetch from 'node-fetch';
-
 export default async (req, res) => {
-  const url = 'https://dev.obtenmas.com/catom/api/challenge/banks';
-
-  try {
-    const apiRes = await fetch(url);
-    if (!apiRes.ok) throw new Error(`HTTP status ${apiRes.status}`);
-    
-    const data = await apiRes.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Error al conectar con la API', details: err.message });
-  }
-};
+    const fetch = (await import('node-fetch')).default;
+    const url = 'https://dev.obtenmas.com/catom/api/challenge/banks';
+  
+    try {
+      const apiRes = await fetch(url);
+      if (!apiRes.ok) throw new Error(`HTTP status ${apiRes.status}`);
+      
+      const data = await apiRes.json();
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).send(data);
+    } catch (err) {
+      res.status(500).json({ error: 'Error al conectar con la API', details: err.message });
+    }
+  };
+  
